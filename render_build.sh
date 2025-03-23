243d6678896385c8a35b2dd3abcdb2b2ed0c55fa
Ayush-Dutta <100012679+Ayush-Dutta@users.noreply.github.com>
2025-03-23 12:27:28 +0530

    Update build process to use apt.txt for git-lfs and normalize line endings

--- render_build.sh
+++ render_build.sh
@@ -1,12 +1,8 @@
-# render-build.sh
 #!/usr/bin/env bash
 
 set -e
 
-# Install Git LFS
-apt-get update && apt-get install -y git-lfs
-
-# Initialize LFS
+# Git LFS should be installed via apt.txt already
 git lfs install
 
 # Pull LFS files
