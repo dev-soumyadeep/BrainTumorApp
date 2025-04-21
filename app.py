493ab21c6764b65295055d80098c412f717e6827
Ayush-Dutta <100012679+Ayush-Dutta@users.noreply.github.com>
2025-04-21 10:07:43 +0530

    Initial commit for Replit deployment

--- app.py
+++ app.py
@@ -165,6 +165,6 @@ def classify(filename):
 
 if __name__ == "__main__":
     # app.run(debug=True, port=5000)
-    if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=3000)
+
 
